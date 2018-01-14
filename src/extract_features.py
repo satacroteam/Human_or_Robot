3aa080d11590cf0bbe8ff01b1635db49e908c754
Florian Robinet <florian.robinet.49000@hotmail.fr>
2018-01-14 21:34:05 +0100

    Feature selection

--- src/extract_features.py
+++ src/extract_features.py
@@ -5,6 +5,7 @@ import os
 import pickle
 import numpy as np
 import pandas as pd
+from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.preprocessing import StandardScaler, LabelBinarizer
 from sklearn_pandas import DataFrameMapper
 
--- src/model.py
+++ src/model.py
@@ -5,6 +5,7 @@ import pickle
 import pandas as pd
 import xgboost as xgb
 import numpy as np
+from matplotlib import pyplot
 
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
@@ -17,9 +18,11 @@ class Model(object):
         self.train_answer = self.load('pickle/train_answer.pkl')
         self.test_data = [test_id for test_id in pd.read_csv("data/test.csv", sep=',')['bidder_id']]
         self.mapper = None
+        self.feature_selection_list = None
         self.model = None
         self.n_comp = 3
         self.pca = None
+        self.t_svd = None
 
     @staticmethod
     def load(pickle_name):
@@ -39,17 +42,38 @@ class Model(object):
             self.pca = PCA(n_components=self.n_comp, random_state=420)
             self.pca.fit(np.nan_to_num(data), answer)
             pca_results_train = self.pca.transform(np.nan_to_num(data))
+
+            # tSVD
+            '''self.t_svd = KernelPCA(n_components=self.n_comp, random_state=420)
+            t_svd_results_train = self.t_svd.fit_transform((np.nan_to_num(data)))'''
+
             data = pd.DataFrame(data)
 
             for i in range(1, self.n_comp + 1):
                 data['pca_' + str(i)] = pca_results_train[:, i - 1]
+                '''data['t_svd_' + str(i)] = t_svd_results_train[:, i - 1]'''
         else:
-            pca2_results_test = self.pca.transform(np.nan_to_num(data))
+
+            pca_results_test = self.pca.transform(np.nan_to_num(data))
+
+            ''' t_svd_results_test = self.t_svd.transform(np.nan_to_num(data))'''
+
             data = pd.DataFrame(data)
+
             for i in range(1, self.n_comp + 1):
-                data['pca_' + str(i)] = pca2_results_test[:, i - 1]
+                data['pca_' + str(i)] = pca_results_test[:, i - 1]
+                '''data['t_svd_' + str(i)] = t_svd_results_test[:, i - 1]'''
 
         return data
+
+    def feature_selection(self, data):
+        return data[
+            [8514, 3699, 8523, 4403, 'pca_1', 8504, 6964, 8506, 2, 8511, 0, 4405, 4406, 4302, 'pca_2', 8516, 3, 8512, 3698,
+             'pca_3', 8510, 8522, 2801, 4345, 6961, 3700, 8503, 8519, 4297, 8508, 8515, 4052, 8517, 4404, 6963, 8521,
+             8524, 4304, 3701, 4301, 8520, 4298, 8509, 8505, 8513, 4303, 7807, 4296, 4397, 4300, 7819, 8507, 4342, 1,
+             4090, 6644, 6962, 3964]
+
+        ]
 
     def train(self):
         """
@@ -59,16 +83,22 @@ class Model(object):
         answer = [int(value) for value in self.train_answer.values()]
         train = self.feature_engineering(train, answer)
 
+        self.feature_selection_list = train.columns.tolist()
+
+        train = pd.DataFrame(data=train, columns=self.feature_selection_list, index=self.train_answer.keys())
+
+        train = self.feature_selection(train)
+
         print(len(train), len(answer))
 
         y_mean = np.mean([int(value) for value in self.train_answer.values()])
         num_boost_rounds = 1250
 
         xgb_params = {
-            'n_trees': 10000,
+            'n_trees': 9000,
             'eta': 0.1,
             'max_depth': 6,
-            'subsample': 0.90,
+            'subsample': 0.89,
             'objective': 'binary:logistic',
             'eval_metric': 'auc',
             'base_score': y_mean,  # base prediction = mean(target)
@@ -78,7 +108,10 @@ class Model(object):
         d_matrix_train = xgb.DMatrix(train, answer)
         self.model = xgb.train(dict(xgb_params, silent=0), d_matrix_train, num_boost_round=num_boost_rounds)
 
+        print(self.model.get_fscore().items())
+
         xgb.plot_importance(self.model)
+        pyplot.show()
 
     def test(self):
         """
@@ -87,6 +120,10 @@ class Model(object):
         """
         test = pd.DataFrame(data=self.train_data, index=self.test_data)
         test = self.feature_engineering(test)
+
+        test = pd.DataFrame(data=test, columns=self.feature_selection_list, index=self.test_data)
+
+        test = self.feature_selection(test)
 
         d_matrix_test = xgb.DMatrix(test)
         y_predicted = self.model.predict(d_matrix_test)
